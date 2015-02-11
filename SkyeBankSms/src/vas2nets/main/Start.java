08fb305c6492c88f5f5dedd39ba6257d577a7285
samson ude <samson ude@172.16.174.128>
2015-02-11 11:01:31 +0100

    11220151100

--- SkyeBankSms/src/vas2nets/main/Start.java
+++ SkyeBankSms/src/vas2nets/main/Start.java
@@ -1,3 +1,5 @@
+package vas2nets.main;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
---
+++ SkyeBankSms/src/vas2nets/main/WriteConfig.java
@@ -0,0 +1,15 @@
+package vas2nets.main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author samson ude
+ */
+public class WriteConfig {
+    
+}
