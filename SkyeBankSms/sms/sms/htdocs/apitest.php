78fdd1f7611e7fb59e837e8ba683deb37e27ac20
Jonathan Ogbimi <jonathan.o@vas2nets.com>
2015-02-17 14:52:41 +0100

    Updates to APItest

--- SkyeBankSms/sms/sms/htdocs/apitest.php
+++ SkyeBankSms/sms/sms/htdocs/apitest.php
@@ -9,10 +9,14 @@ include_once 'lib.php';
 
 #$result = change_password('admin');
 #var_dump(get_user('admin'));
-$result = update_smpp('197.253.10.25','2775', 'kannel2', '12345');
+#$result = update_smpp('197.253.10.25','2775', 'kannel2', '12345');
 #echo $result;
 #var_dump(get_smpp());
 #var_dump(get_plugins());
+
+#var_dump(get_plugin('6'));
+#echo(is_user('admin'));
+#if(is_user('admin2')) echo 'Validated';
 echo test_smpp();
 
 ?>
