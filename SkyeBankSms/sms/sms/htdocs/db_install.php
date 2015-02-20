a4d08b1b431bdd6d650eb13fb127cf9dbed4246d
Jonathan Ogbimi <jonathan.o@vas2nets.com>
2015-02-20 09:06:52 +0100

    20150220906

--- SkyeBankSms/sms/sms/htdocs/db_install.php
+++ SkyeBankSms/sms/sms/htdocs/db_install.php
@@ -35,14 +35,31 @@ $plugin->username='default';
 $plugin->port='3306';
 $plugin->sql_query="Select * from accounts where status='send'";
 $plugin->update_sql="update account set status='sent' where id=$id";
-$plugin->senderid_col='sender';
-$plugin->msg_col='msg';
+$plugin->msg_template="Your account balance is [bal]";
+$plugin->sender_id = 'Skyebank';
 $plugin->id_col='id';
 $plugin->msisdn_col='receiver';
 $plugin->status='0';
 $plugin->user='admin';
 ##
 $pid = R::store($plugin);
+$plugin2 = R::dispense('plugin');
+$plugin2->name='mysql2';
+$plugin2->dbtype='mysql';
+$plugin2->host='localhost';
+$plugin2->pwd = 'password';
+$plugin2->username='root';
+$plugin2->port='3306';
+$plugin2->sql_query="select * from transaction_table where sms_sent = 'no'";
+$plugin2->update_sql="update transaction_table set sms_sent = ? where id = ?";
+$plugin2->msg_template="Your account balance is [balance]";
+$plugin2->sender_id = 'Skyebank';
+$plugin2->id_col='id';
+$plugin2->msisdn_col='receiver';
+$plugin2->status='0';
+$plugin2->user='admin';
+##
+$pid2 = R::store($plugin2);
 echo "Default Plugin added ($pid)<br>";
 echo "Setting up SMPP test Repository....<br>";
 # a table to log result of SMPP test
